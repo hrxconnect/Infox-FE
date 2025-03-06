366dd30c80da778758f27ca09a6c936af065dd7a
MujtabaAhmed2000 <61231702+MuhammadMujtabaAhmed@users.noreply.github.com>
2025-03-06 07:52:49 -0700

    refactor

--- src/Components/Landing/index.js
+++ src/Components/Landing/index.js
@@ -355,7 +355,7 @@ export default function Landing() {
                 gap: 20,
               }}
             >
-              <div style={{ display: "flex" }}>
+              <div style={{ display: "flex", height: 20 }}>
                 <input
                   type="radio"
                   name="selectedSubject"
@@ -372,7 +372,7 @@ export default function Landing() {
                 />
                 <label className="">General Inquiry</label>
               </div>
-              <div style={{ display: "flex" }}>
+              <div style={{ display: "flex", height: 20 }}>
                 <input
                   type="radio"
                   name="selectedSubject"
@@ -389,7 +389,7 @@ export default function Landing() {
                 />
                 <label className="">HR Queries</label>
               </div>
-              <div style={{ display: "flex" }}>
+              <div style={{ display: "flex", height: 20 }}>
                 <input
                   type="radio"
                   name="selectedSubject"
