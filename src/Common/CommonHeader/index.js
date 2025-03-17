9662d433bd3da9620475fd2b10fac9073fed7189
JIYA_LAPTOP\jiyaj <jiyajohn015@gmail.com>
2025-03-17 15:08:02 -0400

    Session ID in Country Selection API - issue fix

--- src/Common/CommonHeader/index.js
+++ src/Common/CommonHeader/index.js
@@ -80,10 +80,9 @@ export default function CommonHeader() {
         }
 
         try {
-            const response = await apiClient.post("/cournty_selection", {
-                selected_country: countryValue,
-                user_id: userId
-            });
+            const response = await apiClient.post("/country_selection/",
+                { selected_country: countryValue }
+            );
     
             console.log('Selected Country API Response:', response.data);
     
@@ -110,9 +109,7 @@ export default function CommonHeader() {
             setTimeout(() => UpdateSelectedCountry(countryValue), 100); // Slightly more delay to ensure state update
         }
     };
-    
-    
-    
+        
     const handleLogout = async () => {
         // Optionally, you can call an API to handle logout on the server side
         // await axios.post("https://app.infox.bot/api/logout/", {}, {
@@ -124,6 +121,7 @@ export default function CommonHeader() {
         // Clear local storage
         localStorage.removeItem("token");
         localStorage.removeItem("userProfile");
+        sessionStorage.clear();
 
         // Navigate to the login page after clearing local storage
         navigate('/login');
